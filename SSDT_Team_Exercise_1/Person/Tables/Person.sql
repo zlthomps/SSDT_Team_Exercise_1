aa42b2d9e0e59b1c9e73609f3dc56eed8f07d6e1
Zach Thompson <zachary.thompson@drivedominion.com>
2015-01-27 16:44:41 -0500

    Adding Tables for Person schema.

---
+++ SSDT_Team_Exercise_1/Person/Tables/Person.sql
@@ -0,0 +1,16 @@
+﻿--Exercise 8a-8e
+
+CREATE TABLE [Person].[Person]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL CONSTRAINT PK_Person_Id PRIMARY KEY CLUSTERED DEFAULT NEWID(), 
+    [IndexId] BIGINT NOT NULL IDENTITY(1,1), 
+    [FName] NVARCHAR(50) NOT NULL, 
+    [LName] NVARCHAR(50) NULL, 
+    [Birthdate] DATE NULL, 
+    CONSTRAINT [CK_Person_ValidateAge] CHECK (DATEDIFF(YEAR,[Birthdate],GETDATE()) >= 18),
+)
+
+
+GO
+
+CREATE INDEX [IX_Person_IndexId] ON [Person].[Person] ([IndexId])
