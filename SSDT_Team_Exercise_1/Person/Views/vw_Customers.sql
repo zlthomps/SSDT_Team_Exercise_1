cddf8ff45fda00a84828150d2a512b667a792836
Zach Thompson <zachary.thompson@drivedominion.com>
2015-01-28 17:43:31 -0500

    Adding View cw_Customers to schema.

---
+++ SSDT_Team_Exercise_1/Person/Views/vw_Customers.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [Person].[vw_Customers]
+	AS
+
+	SELECT Person.FName AS FirstName,
+		   Person.LName AS LastName
+    FROM Person.Customer AS Customer
+	INNER JOIN Person.Person AS Person
+		ON Customer.PersonId = Person.Id
