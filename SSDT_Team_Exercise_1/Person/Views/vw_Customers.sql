602550452e7d89c6653af5add1fcc4106407d000
Zach Thompson <zachary.thompson@drivedominion.com>
2015-02-02 16:12:39 -0500

    Seeding Data, Updates to Customer View, Addition of Opportunity View.

--- SSDT_Team_Exercise_1/Person/Views/vw_Customers.sql
+++ SSDT_Team_Exercise_1/Person/Views/vw_Customers.sql
@@ -1,8 +1,15 @@
 ﻿CREATE VIEW [Person].[vw_Customers]
 	AS
 
-	SELECT Per.FName AS FirstName,
-		   Per.LName AS LastName
-    FROM Person.Customer AS Cust
-	INNER JOIN Person.Person AS Per
-		ON Cust.PersonId = Per.Id
+SELECT  Person.FName AS FirstName ,
+        Person.LName AS LastName ,
+        ISNULL(Email.EmailAddress,N'') AS Email,
+		ISNULL(Phone.PhoneNumber,N'') AS Phone,
+		ISNULL(Address.Street + SPACE(1) + Address.City + N',' + Address.StateProvince + SPACE(1) + Address.PostalCode,N'') AS [Address]
+FROM    Person.Person AS Person
+        LEFT OUTER JOIN Person.Customer AS Customer ON Customer.PersonId = Person.Id
+        LEFT OUTER JOIN Person.CustomerEmail AS Email ON Email.CustomerId = Customer.Id
+		LEFT OUTER JOIN person.CustomerAddress AS [Address] ON Address.CustomerId = Customer.Id
+		LEFT OUTER JOIN Person.CustomerPhone AS Phone ON Phone.CustomerId = Customer.Id
+
+
---
+++ SSDT_Team_Exercise_1/Sales/Views/vw_CustomersOpportunities.sql
@@ -0,0 +1,24 @@
+﻿CREATE VIEW [Sales].[vw_CustomersOpportunities]
+	AS 
+	
+	SELECT 
+		Person.FName,
+		Person.LName,
+		OppType.Name AS OppType,
+		Vehic.Year AS VehicleYear,
+		Make.Name AS Make,
+		Model.Name AS Model,
+		opps.CreatedDate AS OppOpen
+	FROM Person.Person AS Person
+	INNER JOIN Person.Customer AS Cust
+		ON Cust.PersonId = Person.Id
+	INNER JOIN Sales.Opportunity AS Opps
+		ON Opps.CustomerId = Cust.Id
+	INNER JOIN Sales.OpportunityType AS OppType
+		ON OppType.Id = Opps.OpportunityTypeId
+	INNER JOIN Inventory.Vehicle AS Vehic
+		ON Vehic.Id = Opps.VehicleId
+	INNER JOIN Inventory.Make AS Make
+		ON Make.Id = Vehic.MakeId
+	INNER JOIN Inventory.Model AS Model
+		ON Model.Id = Vehic.ModelId
