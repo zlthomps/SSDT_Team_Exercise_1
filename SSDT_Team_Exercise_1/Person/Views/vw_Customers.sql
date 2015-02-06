2eadd872e25f6da7c19d4d6041fdfc33b49cb713
Zach Thompson <zachary.thompson@drivedominion.com>
2015-02-06 14:32:06 -0500

    CustomerAddress and InventoryMake FK refactor

--- SSDT_Team_Exercise_1/Person/Views/vw_Customers.sql
+++ SSDT_Team_Exercise_1/Person/Views/vw_Customers.sql
@@ -5,11 +5,12 @@ SELECT  Person.[FirstName] AS FirstName ,
         Person.[LastName] AS LastName ,
         ISNULL(Email.EmailAddress,N'') AS Email,
 		ISNULL(Phone.PhoneNumber,N'') AS Phone,
-		ISNULL(Address.Street + SPACE(1) + Address.City + N',' + Address.StateProvince + SPACE(1) + Address.PostalCode,N'') AS [Address]
+		ISNULL(Address.Street + SPACE(1) + Address.City + N',' + StateProv.Name + SPACE(1) + Address.PostalCode,N'') AS [Address]
 FROM    Person.Person AS Person
         LEFT OUTER JOIN Person.Customer AS Customer ON Customer.PersonId = Person.Id
         LEFT OUTER JOIN Person.CustomerEmail AS Email ON Email.CustomerId = Customer.Id
 		LEFT OUTER JOIN person.CustomerAddress AS [Address] ON Address.CustomerId = Customer.Id
 		LEFT OUTER JOIN Person.CustomerPhone AS Phone ON Phone.CustomerId = Customer.Id
+		LEFT OUTER JOIN dbo.StateProvince AS StateProv ON [Address].StateProvinceId = StateProv.Id
 
 
